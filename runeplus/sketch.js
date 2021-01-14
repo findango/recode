e5155419088ebee7deb4d7a822ed0a19c970b374
Finlay Cannon <fin@finlaycannon.com>
2021-01-13 21:29:39 -0800

    Experimenting with helper functions for Rune.grid

---
+++ runeplus/sketch.js
@@ -0,0 +1,60 @@
+const colors = {
+    tart_orange: '#F25757',
+    gunmetal: '#2B303A',
+    cornflower_blue: '#7189FF',
+    gold_crayola: '#D7BE82',
+    space_cadet: '#273469',
+    international_orange: '#FF5714',
+};
+
+const width = 520;
+const height = 520;
+const margin = 40;
+
+const r = new Rune({
+    container: 'body',
+    width: width + margin * 2,
+    height: height + margin * 2,
+    debug: false,
+});
+
+r.rect(0, 0, width + 2 * margin, height + 2 * margin).fill('#050A30');
+
+const grid = r.grid({
+    x: margin,
+    y: margin,
+    width: width,
+    height: height,
+    rows: 24,
+    columns: 24,
+    gutter: 0,
+});
+
+const w = grid.state.moduleWidth;
+const h = grid.state.moduleHeight;
+
+randomCells(
+    grid,
+    ({ row, col }) => {
+        const hue = 200 - 4 * row;
+        return r
+            .rect(0, 0, w, h)
+            .stroke('hsv', hue, 70, 68)
+            .strokeWidth(0.5)
+            .fill('none');
+    },
+    0.2,
+);
+randomCells(
+    grid,
+    ({ row, col }) => {
+        const hue = 40 + 4 * row;
+        return r
+            .rect(0, 0, w, h)
+            .stroke('hsv', hue, 70, 68)
+            .fill('hsv', hue, 70, 68);
+    },
+    0.2,
+);
+
+r.draw();
