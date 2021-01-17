b9d23f3fc72be09b74065905a5513f3573497b57
Finlay Cannon <fin@finlaycannon.com>
2021-01-17 14:53:27 -0800

    clean up Structured Squares

--- squares/sketch.js
+++ squares/sketch.js
@@ -2,37 +2,39 @@
 // Originally published in "Computer Graphics and Art" vol1 no3, 1976
 // See: http://recodeproject.com/artwork/v1n3structure-square-series-inwards
 
+const width = 580;
+const height = 580;
+const margin = 60;
+
 const r = new Rune({
-    container: 'body',
-    width: 720,
-    height: 720,
+    container: '#sketch',
+    width: width + 2 * margin,
+    height: height + 2 * margin,
     debug: false,
 });
-
-const margin = 20;
 
 const grid = r.grid({
     x: margin,
     y: margin,
-    width: r.width - 2 * margin,
-    height: r.height - 2 * margin,
-    gutter: 15,
+    width: width,
+    height: height,
+    gutter: 12,
     rows: 16,
     columns: 16,
 });
 
 const shuffle = (array) => array.sort(() => Math.random() - 0.5);
 
-const randomLines = (width, height, count) => {
+const randomLines = (w, h, count) => {
     const lines = [
-        [width / 2, 0, width / 2, height],
-        [0, height / 2, width, height / 2],
-        [0, 0, width, height],
-        [width, 0, 0, height],
-        [width / 2, 0, width, height / 2],
-        [width, height / 2, width / 2, height],
-        [width / 2, height, 0, height / 2],
-        [0, height / 2, width / 2, 0],
+        [w / 2, 0, w / 2, h],
+        [0, h / 2, w, h / 2],
+        [0, 0, w, h],
+        [w, 0, 0, h],
+        [w / 2, 0, w, h / 2],
+        [w, h / 2, w / 2, h],
+        [w / 2, h, 0, h / 2],
+        [0, h / 2, w / 2, 0],
     ];
     return shuffle(lines).slice(0, count);
 };
@@ -40,8 +42,16 @@ const randomLines = (width, height, count) => {
 const makeCell = (width, height, count) => {
     const group = r.group();
     const lines = randomLines(width, height, count);
-    lines.forEach((points) => r.line(...points, group).stroke('#000000'));
-    r.rect(0, 0, width, height, group).fill('none').stroke('#000000');
+    lines.forEach((points) =>
+        r
+            .line(...points, group)
+            .stroke('#273469')
+            .strokeWidth(0.75),
+    );
+    r.rect(0, 0, width, height, group)
+        .fill('none')
+        .stroke('#273469')
+        .strokeWidth(1);
     return group;
 };
 
