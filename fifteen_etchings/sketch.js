4260265d8d3f2ca6a4b24cb787fa3f28920b00ec
Finlay Cannon <fin@finlaycannon.com>
2021-07-13 07:53:59 -0700

    Clean up 15 Etchings

--- fifteen_etchings/sketch.js
+++ fifteen_etchings/sketch.js
@@ -5,25 +5,10 @@ const width = 520;
 const height = 520;
 const margin = 40;
 
-const r = new Rune({
-    container: '#sketch',
-    width: width + 2 * margin,
-    height: height + 2 * margin,
-    debug: false,
-});
-
-const grid = r.grid({
-    x: margin,
-    y: margin,
-    width: width,
-    height: height,
-    rows: 4,
-    columns: 4,
-    gutter: 20,
-});
+const color = '#273469';
 
 // prettier-ignore
-const squares = [
+const tiles = [
     '',   'h',   'r',   'l',
     'v',  'vr',  'vl',  'hr',
     'rl', 'hv',  'hvr', 'hrl',
@@ -37,29 +22,34 @@ const paths = {
     l: (w, h) => new Rune.Line(w, h, 0, 0),
 };
 
-const tile = (w, h, lines = '') => {
-    const color = '#273469';
-    const g = new Rune.Group();
-    lines.split('').forEach((l) => {
-        if (paths[l]) {
-            g.add(paths[l](w, h).stroke(color).strokeWidth(1));
-        }
-    });
-    if (lines) {
-        g.add(
-            new Rune.Rectangle(0, 0, w, h)
-                .fill('none')
-                .stroke(color)
-                .strokeWidth(2),
-        );
-    }
+const r = new Rune({
+    container: '#sketch',
+    width: width + 2 * margin,
+    height: height + 2 * margin,
+    debug: false,
+});
+background(r, 'white');
 
-    return g;
-};
+const grid = r.grid({
+    x: margin,
+    y: margin,
+    width: width,
+    height: height,
+    rows: 4,
+    columns: 4,
+    gutter: 20,
+});
 
 forEachCell(grid, ({ row, col, w, h }) => {
-    const t = (row - 1) * grid.state.columns + (col - 1);
-    return tile(w, h, squares[t]);
+    const i = (row - 1) * grid.state.columns + (col - 1);
+    const g = new Rune.Group();
+    tiles[i].split('').forEach((line) => {
+        g.add(paths[line](w, h).stroke(color).strokeWidth(1));
+    });
+    if (tiles[i]) {
+        g.add(new Rune.Rectangle(0, 0, w, h).fill('none').stroke(color).strokeWidth(2));
+    }
+    return g;
 });
 
 r.draw();
